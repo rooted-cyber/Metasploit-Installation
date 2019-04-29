2a0cd106fa257423e0b90924c70c5aaf724a11e1
Android-Maruf <50019150+Android-Maruf@users.noreply.github.com>
2019-04-29 11:28:19 +0530

    Update Install-metasploit.sh

--- Install-metasploit.sh
+++ Install-metasploit.sh
@@ -5,29 +5,5 @@ s='\033[1;36m'
 echo " "
 echo " "
 echo "$y Installing....... $y"
-apt update
-apt upgrade
-pkg install figlet
-figlet Metasploit
-echo " "
-figlet Installation
-echo "$g Metasploit ab install hone ja rha hai $g"
-echo " "
-sleep 4
-pkg install python
-pkg install python2
-pkg install git
-pkg install curl
-pkg install wget
-pkg install w3m
-pkg install php
-apt-get install unstable-repo
-apt-get install metasploit
-echo " "
-echo " "
-sleep 5
-clear
-echo "$s ...... $s"
-figlet Successfully
-figlet Installed
-echo "$s Ab msfconsole or msfvenom use kar sakte ho $s"
+sh metasploit.sh
+figlet "$y Ab msfconsole se start kar sakte ho $y"
