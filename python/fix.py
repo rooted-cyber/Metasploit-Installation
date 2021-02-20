1c18df5cd0f48c7296c19a63e52a44ed6577d857
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2021-02-20 05:47:09 +0530

    Add files via upload

---
+++ python/fix.py
@@ -0,0 +1,7 @@
+import os
+def hathi():
+	  os.system("cd ~/Metasploit-Installation/javascript;node color.js;node writing.js")
+	  os.system("cd ~/Metasploit-Installation/bash;bash fixing.sh")
+
+
+hathi()
---
+++ python/menu.py
@@ -0,0 +1,10 @@
+import os
+def chappal():
+	print("""
+	\033[1;91m[\033[0m1\033[1;91m]\033[1;93m Install Metasploit
+	\033[1;91m[\033[0m2\033[1;91m]\033[1;93m About Tool
+	\033[1;91m[\033[0m3\033[1;91m]\033[1;93m Exit
+	""")
+
+
+chappal()
