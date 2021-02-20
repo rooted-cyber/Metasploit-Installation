343d44c7754df005ca48d1d6d337795d64616a5c
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2021-02-20 06:10:05 +0530

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -1,14 +1,12 @@
 set() {
+	clear
 	printf "\n\033[1;92m Updating packages..\n"
-	#apt update
-	#apt upgrade
+	apt update
+	apt upgrade
 	for a in php node npm nodejs python toilet figlet ; do
 	if [ -e $PREFIX/bin/$a ];then
-	printf "\n \033[1;93m $a command is install\n\n"
+	printf "\n \033[1;93m $a command is install\n\n\033[0m"
 	sleep 1
-	cd ~/Metasploit-Installation/files
-	dpkg -i Random.deb
-	dpkg -i Font.deb
 	else
 	apt install $a || apt reinstall $a
 	apt install $a || apt install --fix-broken
@@ -18,4 +16,8 @@ set() {
 	fi
 	done
 	}
+	
 	set
+	cd ~/Metasploit-Installation/files
+	dpkg -i Random.deb
+	dpkg -i Font.deb
