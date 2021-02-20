2f92c5be526de87c763c68993739f839adc637a3
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2021-02-20 05:50:02 +0530

    Add files via upload

---
+++ php/detail.php
@@ -0,0 +1,4 @@
+<?php
+system("printf '\033[1;96m This Tool Created by :\033[0m github.com/rooted-cyber\n'");
+system("printf '\033[1;92m Copyright of :\033[0m github.com/noob-hackers\n\n'");
+?>
---
+++ php/write.php
@@ -0,0 +1,3 @@
+<?php
+system("random;printf '\n\n [*] Fixing Metasploit\n\n';sleep 1;bash ~/Metasploit-Installation/bash/ruby.sh");
+?>
